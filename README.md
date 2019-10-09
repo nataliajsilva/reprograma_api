a9b8f9f7272bb55e7e8627294c9545b19c5b1e6c
nataliajsilva <nataliajsilva2@outlook.com>
2019-10-08 22:24:15 -0300

    atualização API

---
+++ README.md
@@ -0,0 +1,1 @@
+### Aprendendo API Meli Reprograma
--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -10,3 +10,13 @@ exports.getById = (request, response) => {
     console.log(id)
     response.status(200).send(alunas.find(aluna => aluna.id == id))
 }
+
+exports.getBooks = (request, response) => {
+    const id = request.params.id
+    const aluna = alunas.find(aluna => aluna.id == id)
+    const livrosAluna = aluna.livros
+    const livrosLidos = livrosAluna.filter(livro => livro.leu == "true") 
+    const tituloLivros = livrosAluna.map(livro => livro.titulo)
+
+    response.status(200).send(livrosLidos)
+}
