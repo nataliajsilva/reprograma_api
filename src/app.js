aabef8135527db4160a3d10c8ede3cb93d451084
nataliajsilva <nataliajsilva2@outlook.com>
2019-10-09 22:19:10 -0300

    atualização aula 3 API

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const app = express ()
 const index = require('./routes/index')
 const alunas = require('./routes/alunasRoute')
 
+app.all('*', function(request,response,next){
+    console.log("Passando pelo app meuuu")
+    next ()
+})
+
 app.use('/', index)
 app.use('/alunas',alunas)
 
--- src/controllers/alunasController.js
+++ src/controllers/alunasController.js
@@ -7,16 +7,33 @@ exports.get = (request,response) => {
 
 exports.getById = (request, response) => {
     const id = request.params.id
-    console.log(id)
+    if(id <= 0 || id > 17){
+        response.redirect(301,"https:google.com.br")
+        //response.send('Id não encontrado na lista de alunas')
+    }
+    //console.log(id)
     response.status(200).send(alunas.find(aluna => aluna.id == id))
 }
 
 exports.getBooks = (request, response) => {
     const id = request.params.id
     const aluna = alunas.find(aluna => aluna.id == id)
+
+    if (!aluna){
+        response.send('Aluna não encontrada')
+    }
     const livrosAluna = aluna.livros
     const livrosLidos = livrosAluna.filter(livro => livro.leu == "true") 
     const tituloLivros = livrosAluna.map(livro => livro.titulo)
 
     response.status(200).send(livrosLidos)
 }
+
+exports.getSp = (request,response) => {
+
+    const nasceuSP = alunas.filter(aluna => aluna.nasceuEmSp == "true")
+    const meninasSp = nasceuSP.map(aluna => aluna.nome)
+
+    response.status(200).send(meninasSp)
+}
+
